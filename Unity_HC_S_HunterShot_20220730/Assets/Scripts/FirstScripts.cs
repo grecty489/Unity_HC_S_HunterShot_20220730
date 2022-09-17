88612627695911d1bf6d1752b71bc18d7a656f6e
grecty489 <110278148+grecty489@users.noreply.github.com>
2022-09-17 17:02:37 +0800

    Update 認識C#

--- Unity_HC_S_HunterShot_20220730/Assets/Scripts/FirstScripts.cs
+++ Unity_HC_S_HunterShot_20220730/Assets/Scripts/FirstScripts.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine;// 引用 unity 引擎遊戲命名空間 ：倉庫實用資料與功能
 
-public class FirstScripts : MonoBehaviour
+namespace grecty489
 {
-    // Start is called before the first frame update
-    void Start()
+    // Unity 類別要執行必須放在遊戲物件上
+    /// <summary>
+    /// 摘要：輔助說明
+    /// First Scripts 第一個腳本 學習 C# 基礎與 Unity
+    /// </summary>
+    public class FirstScripts : MonoBehaviour
     {
+        #region 資料區域
+        // 資料
+        #endregion
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region 事件區域：Unity 入口
+        /// <summary>
+        /// 喚醒事件：遊戲開始時在 Star 之前會執行一次
+        /// </summary>
+        private void Awake()
+        {
+            print("哈囉，World！:D");
+        }
+        /// <summary>
+        /// 開始事件：遊戲開始時在 Awark 之後會執行一次
+        /// </summary>
+        private void Start()
+        {
+            print("開始事件！");
+            // Rich Text
+            print("<color=orange>橘色文字</color>");
+            print("<color=#ff7f50>珊瑚紅</color>");
+        }
+        #endregion
     }
 }
---
+++ Unity_HC_S_HunterShot_20220730/Assets/Scripts/LearnField.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace grecty489
+{
+    /// <summary>
+    /// ¾Ç²ßÄæ¦ì Field
+    /// </summary>
+    public class LearnFrild : MonoBehaviour
+    {
+
+    }
+
+}
+
